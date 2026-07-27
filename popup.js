d543ab7b79afd195e1943205146356bbd51c1846
Rohan S Mirjankar <rohansm668@gmail.com>
2026-07-27 22:53:26 +0530

    Allow addition of custom website urls

--- popup.js
+++ popup.js
@@ -1,6 +1,8 @@
 document.getElementById("addBtn").addEventListener("click", showAddCard);
 document.getElementById("exeAdd").addEventListener("click", exeAddApp);
 
+let url, name, hostName, subDomain;
+
 function showAddCard(){
     document.querySelector(".addCard").hidden = false ;
 }
@@ -10,16 +12,26 @@ function exeAddApp(){
 
 }
 function getAppURL(){
-    let url = document.getElementById("url").value;
-    console.log(url);
-    let name = document.getElementById("name").value;
-    console.log(name);
+    url = document.getElementById("url").value;
+    console.log(`URL: ${url}`);
+    name = document.getElementById("name").value;
+    console.log(`App name : ${name}`);
+    getAppIcon();
+    addApp();
 }
+
+function getAppIcon(){
+    hostName = new URL(url).hostname;
+    console.log(`Host : ${hostName}`);
+    subDomain = hostName.split(".")[0];
+    console.log(`Sub Domain : {subDomain}`);
+}
+
 function addApp(){
     document.querySelector(".main").innerHTML += `<div class="app">
-                <a href="https://github.com/rohan-shridhar" target="_blank">
-                    <img src="https://www.google.com/s2/favicons?domain=github.com&sz=32">
+                <a href="${url}" target="_blank">
+                    <img src="https://www.google.com/s2/favicons?domain=${hostName}&sz=32">
                 </a>
-                <p>Github</p>
+                <p>${name}</p>
             </div>`;
 }
